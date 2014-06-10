466470bed54aa121de1017dd0f8515c4122750a2
Sergey Chikuyonok <serge.che@gmail.com>
2014-06-11 01:38:43 +0400

    Parse color token

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ var old = require('./lib/expression-eval');
 var parser = require('./lib/parser');
 var expressionEval = require('./lib/evaluator');
 
-var expr = '6 / -2';
+var expr = '#fc0 + 3';
 
 // console.log('old eval', original.evaluate(expr));
 // old.parse(expr);
