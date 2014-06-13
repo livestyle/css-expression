7c1b91d46f973a7ebeddadc829314bbdb13d7587
Sergey Chikuyonok <serge.che@gmail.com>
2014-06-13 21:22:37 +0400

    Parse and evaluate color

--- test/expression.js
+++ test/expression.js
@@ -6,10 +6,19 @@ describe('CSS Expression', function() {
 		assert.equal(e('1+2'), 3);
 		assert.equal(e('1 + 2'), 3);
 		assert.equal(e('1 - 2'), -1);
+		assert.equal(e('100 + 12'), 112);
 		assert.equal(e('1 + 2 * 3'), 7);
 		assert.equal(e('(1 + 2) * 3'), 9);
 		assert.equal(e('((1 + 2) * 3) / 9'), 1);
 		assert.equal(e('-3 + 2'), -1);
 		assert.equal(e('6 / -2'), -3);
+	});
+
+	it('should evaluate colors', function() {
+		assert.equal(e('#fc0'), '#ffcc00');
+		assert.equal(e('#111 + #222'), '#333333');
+		assert.equal(e('#111 > #222'), false);
+		assert.equal(e('#111 + 2'), '#131313');
+		assert.equal(e('#111 + 22'), '#272727');
 	});
 });
