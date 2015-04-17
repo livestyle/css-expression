c97a4358c4183cfa1b9cb9700d919796cf02bc85
Sergey Chikuyonok <serge.che@gmail.com>
2015-04-17 18:08:16 +0300

    Added SASS string functions

---
+++ test/functions.js
@@ -0,0 +1,36 @@
+var assert = require('assert');
+var e = require('../index');
+var Context = require('../lib/context');
+
+describe('Functions', function() {
+	it('strings', function() {
+		var ctx = new Context({
+			'@q': '"string"',
+			'@uq': 'string'
+		});
+
+		assert.equal(e('quote(hello)', ctx), '"hello"');
+		assert.equal(e('quote("hello")', ctx), '"hello"');
+		assert.equal(e('quote(@uq)', ctx), '"string"');
+		assert.equal(e('quote(@q)', ctx), '"string"');
+
+		assert.equal(e('unquote(hello)', ctx), 'hello');
+		assert.equal(e('unquote("hello")', ctx), 'hello');
+		assert.equal(e('unquote(@uq)', ctx), 'string');
+		assert.equal(e('unquote(@q)', ctx), 'string');
+
+		assert.equal(e('str-length(@q)', ctx), 6);		
+		assert.equal(e('str-length(@uq)', ctx), 6);
+
+		assert.equal(e('str-insert(abc, d, 1)', ctx), 'adbc');
+		assert.equal(e('str-insert(abc, d, 0)', ctx), 'dabc');
+
+		assert.equal(e('str-index("ababcd", ba)', ctx), 1);
+
+		assert.equal(e('str-slice("abcd", 2)', ctx), '"cd"');
+		assert.equal(e('str-slice(abcd, 2, 3)', ctx), 'c');
+
+		assert.equal(e('to-upper-case(@q)', ctx), '"STRING"');
+		assert.equal(e('to-lower-case(HELLO)', ctx), 'hello');
+	});
+});
